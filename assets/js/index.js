71fb1b8ab7bc66a18c53da215d6032bc4614b935
Javlon1 <muhamedmijotot@gmail.com>
2024-03-01 15:14:09 +0500

    start project

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+// Scroll Up Start
+document.addEventListener('DOMContentLoaded', function () {
+    const scrollUpButton = document.getElementById('scrollUp');
+
+    const handleScroll = () => {
+        const scrollY = window.scrollY;
+        const threshold = 100;
+        if (scrollY > threshold) {
+            scrollUpButton.classList.add('visible');
+        } else {
+            scrollUpButton.classList.remove('visible');
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    scrollUpButton.addEventListener('click', scrollToTop);
+});
+// Scroll Up end
+
+
+//lan start
+function selectItem(item, imgSrc) {
+    document.getElementById('selectedItemText').innerText = item;
+    document.getElementById('selectedItemImage').src = imgSrc;
+}
+//lan end
