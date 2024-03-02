987d553bce6818c87204cb2e174acaa17aeeea40
Javlon1 <muhamedmijotot@gmail.com>
2024-03-02 16:08:29 +0500

    header end

--- assets/js/index.js
+++ assets/js/index.js
@@ -31,3 +31,27 @@ function selectItem(item, imgSrc) {
     document.getElementById('selectedItemImage').src = imgSrc;
 }
 //lan end
+
+
+// nav start
+const menu = document.getElementById("menu");
+const navList = document.querySelector(".nav__list");
+const navContact = document.querySelector(".nav__contact");
+const opacity = document.querySelector(".opacity");
+
+menu.onclick = () => {
+    menu.classList.toggle("openmenu");
+    navList.classList.toggle("active_nav");
+    navContact.classList.toggle("active_nav");
+    opacity.classList.toggle("opacity_act");
+    document.body.style.overflow = document.body.style.overflow === 'hidden' ? 'auto' : 'hidden';
+};
+
+opacity.onclick = () => {
+    menu.classList.remove("openmenu");
+    navList.classList.remove("active_nav");
+    navContact.classList.remove("active_nav");
+    opacity.classList.remove("opacity_act");
+    document.body.style.overflow = 'auto';
+};
+// nav end
